f9603998ec876c7bc409d91e0738e98895a6214b
Konrad Napieraj <wrx93998@student.wroclaw.merito.pl>
2026-06-07 15:48:47 +0200

    Workflows V4

--- cli.py
+++ cli.py
@@ -1,7 +1,6 @@
 # cli.py
 from logic.rectangle import area, perimeter
 
-
 def main():
     print("Program do obliczania prostokąta")
     width = float(input("Podaj szerokość: "))
@@ -9,7 +8,6 @@ def main():
 
     print(f"Pole prostokąta: {area(width, height)}")
     print(f"Obwód prostokąta: {perimeter(width, height)}")
-
 
 if __name__ == "__main__":
     main()
